b7468a19fc1c94b6e3f8392407645b5033e56972
Junghoon Lee <scarlet3628@likelion.org>
2019-02-12 09:01:44 +0000

    today's last

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -318,6 +318,47 @@ $(document).ready(function(){
   }, getRandomArbitrary(100, 500));
   
   
+  
+  //Begin sort by price&percentage on promo code
+    function sortMeBy(arg, sel, elem, order) {
+    var $selector = $(sel),
+      $element = $selector.children(elem);
+  
+    $element.sort(function(a, b) {
+      var an = parseInt(a.getAttribute(arg)),
+        bn = parseInt(b.getAttribute(arg));
+  
+      if (order == 'asc') {
+        if (an > bn)
+          return 1;
+        if (an < bn)
+          return -1;
+      } else if (order == 'desc') {
+        if (an < bn)
+          return 1;
+        if (an > bn)
+          return -1;
+      }
+      return 0;
+    });
+  
+    $element.detach().appendTo($selector);
+    }
+    
+    $(document).on('click', '.sort1', function() {
+      sortMeBy('data-price', '.sort-list', 'div.card', 'desc');
+    });
+    $(document).on('click', '.sort2', function() {
+      sortMeBy('data-price', '.sort-list', 'div.card', 'asc');
+    });
+    $(document).on('click', '.sort3', function() {
+      sortMeBy('data-score', '.sort-list', 'div.card', 'desc');
+    });
+    $(document).on('click', '.sort4', function() {
+      sortMeBy('data-score', '.sort-list', 'div.card', 'asc');
+    });
+  //End
+  
 
 });
 
