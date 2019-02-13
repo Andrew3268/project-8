40ccddb61cdd2e14a8ec000c736f36bec040e664
Junghoon Lee <scarlet3628@likelion.org>
2019-02-13 09:00:51 +0000

    deals

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -91,102 +91,8 @@ $(document).ready(function(){
     });
   //End
 
-  //Begin show side menu on gadgets page
-    var document_gadgets = $(document).height();
-    //console.log(document_gadgets);
-    $(".gadgets_wrapper main section#filter").height(document_gadgets);
-    
-    $(".gadgets_wrapper main .gnb_open").on("click", function(){
-      $(".gadgets_wrapper main .gnb_kbg").fadeIn(300);
-      $(".gadgets_wrapper main section#filter").animate({
-        right : 0
-      },300);
-    });
-    
-    $(".gadgets_wrapper main .gnb_close").on("click", function(){
-      $(".gadgets_wrapper main .gnb_kbg").fadeOut(300);
-      $(".gadgets_wrapper main section#filter").animate({
-        right : -800
-      },300);
-    });
-    
-     $(".gadgets_wrapper main .gnb_kbg").on("click", function(){
-      $(".gadgets_wrapper main .gnb_kbg").fadeOut(300);
-      $(".gadgets_wrapper main section#filter").animate({
-        right : -800
-      },300);
-    });
-
-    $(".gadgets_wrapper main .gnb_open_b").on("click", function(){
-      $(".gadgets_wrapper main .gnb_kbg").fadeIn(300);
-      $(".gadgets_wrapper main section#filter").animate({
-        right : 0
-      },300);
-    });
-
-    $(".gadgets_wrapper main .gnb_kbg_b").on("click", function(){
-      $(".gadgets_wrapper main .gnb_kbg").fadeOut(300);
-      $(".gadgets_wrapper main section#filter").animate({
-        right : -800
-      },300);
-    });   
-  //End
-
-  //Begin Opne&Close Menu on promo_code
-  var menu_status;
-  $(".codes .lt_cnt .toggle>ul>li>a").on("click", function(){
-    menu_status = $(this).parent().hasClass("on");
-    //console.log(menu_status);
-    if(menu_status == true){
-      //지금 열려있는 상태 -- 닫는기능
-      $(this).parent().removeClass("on");
-      $(this).next().slideUp();
-    }else{
-      //지금 닫혀있는 상태 - 여는기능
-      $(".codes .lt_cnt .toggle>ul>li").removeClass("on");
-      $(".codes .lt_cnt .toggle>ul>li>ul").slideUp();
-      $(this).parent().addClass("on");
-      $(this).next().slideDown();
-    }
-  });
-  //End
-
-
-  //Begin show side menu on gadgets page
-    var document_deals = $(document).height();
-      //console.log(document_deals);
-      $(".sub_nav .gnb_m").height(document_deals);
-      
-      $(".sub_nav .gnb_open").on("click", function(){
-        $(".sub_nav .gnb_kbg").fadeIn(300);
-        $(".sub_nav .gnb_m").animate({
-          right : 0
-        },300);
-      });
-      
-      $(".sub_nav .gnb_close").on("click", function(){
-        $(".sub_nav .gnb_kbg").fadeOut(300);
-        $(".sub_nav .gnb_m").animate({
-          right : -800
-        },300);
-      });
-
-      $(".sub_nav .gnb_open_b").on("click", function(){
-        $(".sub_nav .gnb_kbg").fadeIn(300);
-        $(".sub_nav .gnb_m").animate({
-          right : 0
-        },300);
-      });
-      
-      $(".sub_nav .gnb_close").on("click", function(){
-        $(".sub_nav .gnb_kbg").fadeOut(300);
-        $(".sub_nav .gnb_m").animate({
-          right : -1000
-        },300);
-      });
-  //End
-
-  //Begin js for Deals page
+  
+  //Begin highlight menu
     function fun_nav(fun_name){
       $(fun_name).each(function(){
        if ($(this).attr("href") == window.location.pathname){
@@ -211,83 +117,8 @@ $(document).ready(function(){
     //End 
   //End
   
-  
-  //Begin filter on featured
-  var example = new DataFilterJS({
-    filterGroup: '.filter-group',
-    filterNode: '.filter-node',
-    filterButtonGroup: '.filter-search',
-    transitionSpeed: '0'
-  });
 
-  function DataFilterJS(config){
-    // define variables
-    var filterGroup = config.filterGroup;
-    var filterNode = config.filterNode;
-    var filterButtonGroup = config.filterButtonGroup;
-    var transitionSpeed = config.transitionSpeed;
-    var filter_group = $(filterGroup);
-    var filter_node = $(filterGroup+' > '+filterNode);
-    var search_val = $(filterButtonGroup+'> '+'input[type="text"]').value;
-    $(filterGroup+', '+filterNode+', '+filterNode+'.filter-me').css({"transition":"all "+transitionSpeed});
-    
-    // (Button) Data Filter Function
-    $(filterButtonGroup+' .filter-button').on('click', function(e) {
-      var eventFilter = e.currentTarget.attributes[1].nodeValue;
-       // Loop through Filter Nodes on Button Click
-       $.each(filter_node, function(i, v) {
-          if(eventFilter == "*") {
-            filter_node[i].classList.remove('filter-me');
-          } else if(filter_node[i].getAttribute('data-filter').includes(eventFilter) == false) {
-            filter_node[i].classList.add('filter-me')
-          } else {filter_node[i].classList.remove('filter-me');}
-       });
-    }); // end click event
-
-    // (Keystroke) Data Filter Function    
-    $(filterButtonGroup+' > input[type="text"]').on('keypress keydown keyup', function(e, fnd) {
-      // Loop through Filter Nodes on Keystroke
-      $.each($(filterNode), function(i, v) {
-        var node = v.getAttribute("data-filter").toString().toUpperCase();
-        var search_string = $(filterButtonGroup+' > '+'input[type="text"]').val().toUpperCase();
-        //console.log(node.getAttribute('data-filter'));
-        if(node.includes(search_string) == false) {
-          v.classList.add('filter-me');
-        } else {v.classList.remove('filter-me');}
-      }); // end each
-    }); // end keypress
-  }
-  //End filter on featured
-  
-  
-  //Begin show menu on featured
-  	var document_h = $(document).height();
-	//console.log(document_h);
-	$(".featured .sm_mobile").height(document_h);
-	
-	$(".featured .gnb_open").on("click", function(){
-		$(".featured .gnb_kbg").fadeIn(300);
-		$(".featured .sm_mobile").animate({
-			right : 0
-		},300);
-
-	});
-	$(".featured .gnb_kbg").on("click", function(){
-		$(".featured .gnb_kbg").fadeOut(300);
-		$(".featured .sm_mobile").animate({
-			right : -800
-		},300);
-	});
-	$(".featured .gnb_close").on("click", function(){
-		$(".featured .gnb_kbg").fadeOut(300);
-		$(".featured .sm_mobile").animate({
-			right : -800
-		},300);
-	});
-  //End show menu on featured
-  
-  
-  
+ //Begin Loading page
   function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
   }
@@ -316,48 +147,9 @@ $(document).ready(function(){
       setTimeout(function () {return $('.loading').css('transform', 'translateY(calc(100% + 10px))');}, 400);
     }
   }, getRandomArbitrary(100, 500));
+//End
   
   
-  
-  //Begin sort by price&percentage on promo code
-    function sortMeBy(arg, sel, elem, order) {
-    var $selector = $(sel),
-      $element = $selector.children(elem);
-  
-    $element.sort(function(a, b) {
-      var an = parseInt(a.getAttribute(arg)),
-        bn = parseInt(b.getAttribute(arg));
-  
-      if (order == 'asc') {
-        if (an > bn)
-          return 1;
-        if (an < bn)
-          return -1;
-      } else if (order == 'desc') {
-        if (an < bn)
-          return 1;
-        if (an > bn)
-          return -1;
-      }
-      return 0;
-    });
-  
-    $element.detach().appendTo($selector);
-    }
-    
-    $(document).on('click', '.sort1', function() {
-      sortMeBy('data-price', '.sort-list', 'div.card', 'desc');
-    });
-    $(document).on('click', '.sort2', function() {
-      sortMeBy('data-price', '.sort-list', 'div.card', 'asc');
-    });
-    $(document).on('click', '.sort3', function() {
-      sortMeBy('data-score', '.sort-list', 'div.card', 'desc');
-    });
-    $(document).on('click', '.sort4', function() {
-      sortMeBy('data-score', '.sort-list', 'div.card', 'asc');
-    });
-  //End
   
 
 });
